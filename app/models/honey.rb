f86ae8b16250f708eb920bb983dd8164232066b8
Minwoo Lee <ermaker@gmail.com>
2015-08-03 01:05:28 +0900

    Add Honey

---
+++ app/models/honey.rb
@@ -0,0 +1,8 @@
+require 'mongoid'
+
+module Honeypot
+  # The Container for anything
+  class Honey
+    include Mongoid::Document
+  end
+end
---
+++ spec/app/models/honey_spec.rb
@@ -0,0 +1,11 @@
+require 'app'
+require 'models/honey'
+
+RSpec.describe Honeypot::Honey do
+  it 'saves anything' do
+    described_class.new(a: 3, b: 4).save
+    expect(described_class.all.size).to eq(1)
+    expect(described_class.first.a).to eq(3)
+    expect(described_class.first.b).to eq(4)
+  end
+end
